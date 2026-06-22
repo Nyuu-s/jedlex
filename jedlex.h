4372f4b5c5e8c16a64e1c69ed82fc94b5b60d0f2
Edgar Jacquemoud <cookiesarenightmares@gmail.com>
2026-06-23 00:54:39 +0200

    fixed bug on shift for first byte mask

--- jedlex.h
+++ jedlex.h
@@ -4,22 +4,23 @@
 #define JEDLEX_H
 #include <stddef.h>
 
+
 #if defined(LEX_ENCODING_UTF8)
-    #define LEX_DECODE(p, out_cp, out_len) utf8_decode(p, out_cp, out_len)
+    #define LEX_DECODE(p, out_cp) utf8_decode(p, out_cp)
 #elif defined(LEX_ENCODING_UTF16_LE)
-    #define LEX_DECODE(p, out_cp, out_len) utf16le_decode(p, out_cp, out_len)
+    #define LEX_DECODE(p, out_cp) utf16le_decode(p, out_cp)
 #elif defined(LEX_ENCODING_UTF16_BE)
-    #define LEX_DECODE(p, out_cp, out_len) utf16be_decode(p, out_cp, out_len)
+    #define LEX_DECODE(p, out_cp) utf16be_decode(p, out_cp)
 #elif defined(LEX_ENCODING_UTF32_LE)
-    #define LEX_DECODE(p, out_cp, out_len) utf32le_decode(p, out_cp, out_len)
+    #define LEX_DECODE(p, out_cp) utf32le_decode(p, out_cp)
 #elif defined(LEX_ENCODING_UTF32_BE)
-    #define LEX_DECODE(p, out_cp, out_len) utf32be_decode(p, out_cp, out_len)
+    #define LEX_DECODE(p, out_cp) utf32be_decode(p, out_cp)
 #elif defined(LEX_ENCODING_EBCDIC_BE)
-    #define LEX_DECODE(p, out_cp, out_len) ebcdicbe_decode(p, out_cp, out_len)
+    #define LEX_DECODE(p, out_cp) ebcdicbe_decode(p, out_cp)
 #elif defined(LEX_ENCODING_EBCDIC_LE)
-    #define LEX_DECODE(p, out_cp, out_len) ebcdicle_decode(p, out_cp, out_len)
+    #define LEX_DECODE(p, out_cp) ebcdicle_decode(p, out_cp)
 #else
-    #define LEX_DECODE(p, out_cp, out_len) utf8_decode(p, out_cp, out_len)
+    #define LEX_DECODE(p, out_cp) utf8_decode(p, out_cp)
 #endif
 
 // #### GLOBALS
@@ -76,6 +77,7 @@ typedef struct {
   // uint8 length; just compute it from cp  
 } JCodePoint;
 
+Elex_status utf8_decode(JLexer* lex, JCodePoint* cp);
 
 // #### codepoint classifier | merge directly in decoder, might change in the future
 // #### sequence classifier (fsm)
@@ -86,21 +88,60 @@ int get_cp_len(codepoint_t cp){
   else return -1;
 }
 
-int  decode_and_classify_uni(JLexer* lex, JCodePoint* cp){
-  if(lex->status != JED_READY) { return -1;}
+Elex_status jlex_get_char(JLexer* lex, JCodePoint* cp){
+  LEX_DECODE(lex, cp);
+  return lex->status;
+}
+
+Elex_status utf8_decode(JLexer* lex, JCodePoint* cp){
+  if(lex->status != JED_READY) { lex->status = JED_ERROR; return lex->status;}
   
-  if(*lex->buff < 0x80) {cp->value = *lex->buff; return 1;}
+  uint8* cur = lex->buff + lex->b_offset;
 
-  uint8* p = lex->buff;
-  int len = get_cp_len(*p);
-  if(len <= 0 || lex->b_offset + len > lex->buff_size) { lex->status = JED_ERROR; return -1;}
-
-  uint8 first_byte_mask = ~(0xFF << (7 - len));
-
-  cp->value |= ((*p & first_byte_mask) << (6 * (len-1)));
-  for(int i = 1; i < len; i++){
-    cp->value |= (( *(p+i) & 0x3F) << (6 * (len-i-1)));
+  if(lex->b_offset >= lex->buff_size){
+    lex->status = JED_EOF;
+    return lex->status;
   }
+
+  if(*cur < 0x80) {
+    cp->value = *lex->buff;
+    lex->b_offset++;
+    lex->c_offset++;
+    return lex->status;
+  }
+  printf("hello there\n");
+
+  int len = get_cp_len(*cur);
+  if(len <= 0 ) {
+    lex->status = JED_ERROR; 
+    return lex->status;
+  }
+
+  if(lex->b_offset + len > lex->buff_size){
+    lex->status = JED_EOF;
+    // might span on multiple buffers
+  }
+  // 1100 0000 if 1 since 1 bit header 7 payload
+  // 1110 0000 if 2 since 3 bit header 5 payload
+  // 1111 0000 if 3 since 4 bit header 4 payload
+  // 1111 1100 if 4 since 5 bit header 3 payload
+  // using len * (len != 1) so shift by 7 when len=1 otherwise shift by 7-(2;4)
+  uint8 first_byte_mask = ~(0xFF << (7 - (len * (len != 1));
+
+
+  // 0000 0000 | (cur & fb_mask) << (6 * (1;4) 
+  // zero out the header bits in current byte (& operation) 
+  // then move them to the left enough so there is space for len-1 payload
+  // then read len-1 bytes and remove their header and slide them left in the result in their corresponding slot
+  cp->value |= ((*cur & first_byte_mask) << (6 * (len-1)));
+  for(int i = 1; i < len; i++){
+    cp->value |= (( *(cur+i) & 0x3F) << (6 * (len-i-1)));
+  }
+
+  //move byte offset and codepoint offset;
+  lex->b_offset += len;
+  lex->c_offset++;
+  return lex->status;
 
 }
 void get_next_token();
